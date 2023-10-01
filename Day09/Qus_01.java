7d491843563cd291436e56072782edcd4d42accd
SRBBHX <saurabhxyz50@gmail.com>
2023-10-01 08:02:10 +0530

    Updated

---
+++ Day09/Qus_01.java
@@ -0,0 +1,6 @@
+package Day09;
+public class Qus_01{
+        public static void main(String[] args) {
+                
+        }
+}
---
+++ Day09/Qus_02.java
@@ -0,0 +1,5 @@
+package Day09;
+
+public class Qus_02 {
+        
+}
